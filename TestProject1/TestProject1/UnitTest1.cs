6f71b67962a0a9777782fe6dd7bbcb7f1dde4c11
František Brikner <briknerf22@spstrutnov.cz>
2024-04-10 14:04:40 +0200

    test01

--- TestProject1/TestProject1/UnitTest1.cs
+++ TestProject1/TestProject1/UnitTest1.cs
@@ -9,5 +9,54 @@ namespace TestProject1
         public void TestMethod1()
         {
         }
+        [TestClass]
+        public class StackTests
+        {
+            private Stack stack;
+
+            [TestInitialize]
+            public void Setup()
+            {
+                stack = new Stack();
+            }
+
+            [TestMethod]
+            public void TestPushPop()
+            {
+                stack.Push(1);
+                stack.Push(2);
+                Assert.AreEqual(2, stack.Pop());
+                Assert.AreEqual(1, stack.Pop());
+            }
+
+            [TestMethod]
+            public void TestPeek()
+            {
+                stack.Push(1);
+                stack.Push(2);
+                Assert.AreEqual(2, stack.Peek());
+                Assert.AreEqual(2, stack.Size());
+                stack.Pop();
+                Assert.AreEqual(1, stack.Peek());
+                Assert.AreEqual(1, stack.Size());
+            }
+
+            [TestMethod]
+            public void TestIsEmpty()
+            {
+                Assert.IsTrue(stack.IsEmpty());
+                stack.Push(1);
+                Assert.IsFalse(stack.IsEmpty());
+                stack.Pop();
+                Assert.IsTrue(stack.IsEmpty());
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(IndexOutOfRangeException))]
+            public void TestPopEmptyStack()
+            {
+                stack.Pop();
+            }
+        }
     }
 }
